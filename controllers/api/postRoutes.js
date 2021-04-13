7a021c9e56e9f27db9d1912a196ea1ce8969a245
jagg1991 <josephguajardo@ymail.com>
2021-04-13 13:20:22 -0700

    trying to get post to user to work

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,21 +1,17 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
-const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+
+router.post('/', async (req, res) => {
     try {
-        const newPost = await Post.create({
-            ...req.body,
-            user_id: req.session.user_id,
-        });
+        const newPost = await Post.create(req.body);
 
         res.status(200).json(newPost);
     } catch (err) {
         res.status(400).json(err);
     }
 });
-
-router.delete('/:id', withAuth, async (req, res) => {
+router.delete('/:id', async (req, res) => {
     try {
         const postData = await Post.destroy({
             where: {
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,20 +1,23 @@
 const sequelize = require('../config/connection');
-const { User, Post } = require('../models');
+
+const User = require('../models/User');
+const Post = require('../models/Post');
+
 
 const userData = require('./userData.json');
 const postData = require('./postData.json');
 
 
+// Add the `async` keyword to the function `seedDatabase` to make Asynchronous.
 const seedDatabase = async () => {
+
+    // Add the `await` keyword infront of the expressions inside the `async` function.
     await sequelize.sync({ force: true });
 
-    await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true,
-    });
-    await Post.bulkCreate(postData, {
-        returning: true,
-    })
+    // Once JavaScript recogonizes the `await` keyword it waits for the promise to be fufilled before moving on.
+    const user = await User.bulkCreate(userData);
+    const post = await Post.bulkCreate(postData);
+
 
 
     process.exit(0);
