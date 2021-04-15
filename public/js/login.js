ef5e50a46b14c02e1cfc480c4280ba604b967492
jagg1991 <josephguajardo@ymail.com>
2021-04-14 23:15:28 -0700

    adding log in, and post

--- public/js/login.js
+++ public/js/login.js
@@ -2,14 +2,14 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
 
     // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
+    const username = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
-    console.log(email)
-    if (email && password) {
+
+    if (username && password) {
         // Send a POST request to the API endpoint
         const response = await fetch('/api/users/login', {
             method: 'POST',
-            body: JSON.stringify({ username: email, password: password }),
+            body: JSON.stringify({ username: username, password: password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
@@ -21,3 +21,8 @@ const loginFormHandler = async (event) => {
         }
     }
 };
+
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
